b14f82c0f572f4ae82de3af5ecc7d2900740e4da
enqilin <114181724+enqilin@users.noreply.github.com>
2022-12-01 10:48:07 +0000

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,24 @@
+"""Dada una matriz de números enteros N, encuentre la suma de sus elementos.
+Función descriptiva
+Complete la función simpleArraySum . Debe devolver la suma de los elementos de la matriz como un 
+número entero.
+simpleArraySum tiene los siguientes parámetros:
+ar : una matriz de números enteros
+Formato de entrada
+La primera línea contiene un número entero, N, que denota el tamaño de la matriz.
+La segunda línea contiene N enteros separados por espacios que representan los elementos de la 
+matriz."""
+# Complete la función simpleArraySum
+def simpleArraySum(ar):
+    N = length(ar)
+    N=
+
+
+    
+    return 
+# Prueba de código
+if __name__ == '__main__':
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+    print(result)
